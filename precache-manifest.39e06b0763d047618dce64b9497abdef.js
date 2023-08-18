139ad080637b88187e16cff7c455c9310a747a0b
Athena Chiarello <107880032+athenabean@users.noreply.github.com>
2023-08-18 12:52:23 -0400

    Updates

---
+++ precache-manifest.39e06b0763d047618dce64b9497abdef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5fdecddb09e833d6a173",
+    "url": "/portfolio/static/css/main.0884b4ad.chunk.css"
+  },
+  {
+    "revision": "5fdecddb09e833d6a173",
+    "url": "/portfolio/static/js/main.5fdecddb.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/portfolio/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "48adbe2b1f9d49f255f3a8e8456d61d9",
+    "url": "/portfolio/index.html"
+  }
+];
